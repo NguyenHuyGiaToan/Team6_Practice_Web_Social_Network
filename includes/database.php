7f676231f1251cdeebbf94e28824747c46d4b87b
NguyenHuyGiaToan <nhgtoan215@gmail.com>
2026-01-06 14:28:01 +0700

    Chỉnh sửa file database.php: thay PDO thành mysqli/Thêm thư mục Style-css vào assets

--- includes/database.php
+++ includes/database.php
@@ -1,22 +1,16 @@
 <?php
-// Thông số kết nối Database
-$host = 'localhost';
-$db_name = 'WEBMXH_Nhom06'; 
-$username = 'root';
-$password = '';
-
-try {
-    // Khởi tạo kết nối PDO
-    $conn = new PDO("mysql:host=$host;dbname=$db_name;charset=utf8", $username, $password);
+    $servername = "localhost";
+    $username = "root";
+    $password = ""; 
+    $dbname = "WEBMXH_Nhom06";
     
-    // Thiết lập chế độ báo lỗi
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   
+    $conn = mysqli_connect($servername, $username, $password,$dbname);
+    mysqli_set_charset($conn , 'UTF8');
     
-    // Thiết lập kiểu trả về dữ liệu mặc định là mảng kết hợp (Associative Array)
-    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-
-} catch(PDOException $e) {
-    // Nếu kết nối lỗi, dừng chương trình và báo lỗi
-    die("Kết nối cơ sở dữ liệu thất bại: " . $e->getMessage());
-}
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+    
+    
 ?>
